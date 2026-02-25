a35067f2c73f1870508f92629e9d9eb1ebd5dea1
Pawan Kumar <55574659+pawankum0010@users.noreply.github.com>
2026-02-25 08:06:50 +0530

    Add SQL commands for altering large tables
    Added SQL commands for altering large tables in MySQL.

---
+++ mysql/alter_large_table.sql
@@ -0,0 +1,12 @@
+-- 8.0.12 or newer
+ALTER TABLE your_table_name 
+ADD COLUMN new_column_name DATATYPE DEFAULT NULL,
+ALGORITHM=INSTANT,LOCK=NONE;
+
+-- 5.6 or higher
+ALTER TABLE your_table_name 
+CHANGE COLUMN old_name new_name DATATYPE,
+ALGORITHM=INPLACE, 
+LOCK=NONE;
+-- Percona Online Schema Change (pt-osc)
+pt-online-schema-change --execute --alter "MODIFY COLUMN status VARCHAR(50)" D=your_db,t=your_table
